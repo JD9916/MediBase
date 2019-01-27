5f48832534c6d732950cc8aabf6420c09d65b37d
Jeremy Davis <jeremy.davis@mail.mcgill.ca>
2019-01-27 04:34:56 -0500

    adding files

--- Database.php
+++ Database.php
@@ -1,11 +1,13 @@
 <html>
   <head>
-    <meta http-equiv="Refresh" content="0; url=http://localhost/login.html" />
+    <meta http-equiv="Refresh" content="0; url=http://localhost/login.php" />
   </head>
 </html>
 
 <?php
      $Database= mysqli_connect("localhost", "root", "root", "websiteusers");
+     $username = $_POST['username'];
+     $password = $_POST['password'];
      $first= $_POST['name_first'];
      $middle = $_POST['name_middle'];
      $last = $_POST['name_last'];
@@ -21,7 +23,12 @@
 
      echo $first . " " . $middle . " " . $last . " " . $email . " " . $phone . " " . $city . " " . $address . " ". $dateOfBirth . " ";
      echo $monthOfBirth . " " . $yearOfBirth . " " . $gender;
-     $query="INSERT INTO websiteusers(firstname, middlename, lastname, email, phone, city, address, dateOfBirth, monthOfBirth, yearOfBirth, gender) VALUES ('$first', '$middle', '$last', '$email','$phone', '$city', '$address', '$dateOfBirth', '$monthOfBirth', '$yearOfBirth', '$gender')";
+     echo "username: " . $username . " password: " . $password;
+     $query="INSERT INTO websiteusers(firstname, middlename, lastname, email, phone, city, address, dateOfBirth, monthOfBirth, yearOfBirth, gender, username, password) VALUES ('$first', '$middle', '$last', '$email','$phone', '$city', '$address', '$dateOfBirth', '$monthOfBirth', '$yearOfBirth', '$gender')";
+     $sth = $Database->prepare($query);
+     $sth->execute();
+
+     $query="INSERT INTO websiteusers(username, password) VALUES ('$username', '$password')";
      $sth = $Database->prepare($query);
      $sth->execute();
 ?>
